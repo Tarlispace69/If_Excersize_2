59f2761ece2b1ac4d39434bddab79ef3287fec88
user <user@ZC415014>
2023-01-10 08:43:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace If_Excersize_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring variables
+            int input1, input2;
+
+            //recieving first input
+            Console.Write("Please enter a number: ");
+            input1 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(""); //newline
+
+            //recieving second input
+            Console.Write("Please enter another number: ");
+            input2 = int.Parse(Console.ReadLine());
+
+            if (input1>0 && input2>0)
+            {
+                Console.WriteLine($"The product of inputs is: {input1 * input2}");
+            }
+        }
+    }
+}
